e2b597f09ffb4dff8229b80b76302490d11a4231
Harsh Khandelwal <khandelwalharsh401@gmail.com>
2022-11-26 16:41:00 +0530

    Create Exceptionally_odd.cpp

---
+++ Exceptionally_odd.cpp
@@ -0,0 +1,12 @@
+class Solution{   
+public:
+    int getOddOccurrence(int arr[], int n) 
+    {
+        int ans = 0;
+        for(int i = 0; i < n; i++)
+        {
+            ans = ans ^ arr[i];
+        }
+        return ans;
+    }
+};
