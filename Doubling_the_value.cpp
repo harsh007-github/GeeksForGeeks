6f2dbae75c360cc6de1bd539291f6d2a9efc369d
Harsh Khandelwal <khandelwalharsh401@gmail.com>
2022-11-27 13:30:39 +0530

    Create Doubling_the_value.cpp

---
+++ Doubling_the_value.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+    public:
+    long long solve(int n, long long a[], long long b)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            if(a[i] == b)
+            {
+                b *= 2;
+            }
+        }
+        return b;
+    }  
+};
