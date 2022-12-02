d67390658c02bc4d5460a365f4e362498332b4f8
Harsh Khandelwal <khandelwalharsh401@gmail.com>
2022-12-03 00:14:51 +0530

    Create Nth_term_in_series.cpp

---
+++ Nth_term_in_series.cpp
@@ -0,0 +1,10 @@
+// User function Template for C++
+class Solution {
+  public:
+    long long int nthOfSeries(long long int n)
+    {
+        long long int ans = (8*n)*n + 1;
+        return ans;
+        
+    }
+};
